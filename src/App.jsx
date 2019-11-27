9d7cc43a5b239f98497575c3decff5dbe722bef2
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2019-11-27 18:27:24 +0300

    Add Apollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const EXCHANGE_RATES = gql`{
+  popular_artists {
+    artists {
+      id
+      name
+    }
+  }
+}`;
 
 function App() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <div className="App">
-      test
+      <ul>
+        {data.popular_artists.artists.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
